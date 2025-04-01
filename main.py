fa7a0483d49c867e867f29bd7ca4d054f1aec82e
batonandadrian <134459797+batonandadrian@users.noreply.github.com>
2025-04-01 03:24:58 +0000

    Apr 1, 2025, 3:24 AM

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ import os
 #print(Fore.RED + 'This text is red in color')
 def main():
     menu()
-def menu():
-    menu_result = input('Welcome to Chess 4960!\n1. Open instructions\n2. Start new game\nEnter what you want to do: ')
 
-print('asdasd')
+def clear():
+    os.system('clear')
+
+def instructions():
+    clear()
+    
+def menu():
+    menu_result = input('Welcome to Chess 960!\n1. Open instructions\n2. Start new game\nEnter what you want to do: ')
+    while menu_result not in '12':
+        clear()
+        menu_result = input('Welcome to Chess 960!\n1. Open instructions\n2. Start new game\nEnter what you want to do: ')
+    if menu_result == '1':
+        instructions()
+menu()
