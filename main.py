f840fecdc1aa8298f483791c4efba3e56d893549
batonandadrian <134459797+batonandadrian@users.noreply.github.com>
2025-06-06 01:54:21 +0000

    Added structuring and logic for the game, added blank subroutines

--- main.py
+++ main.py
@@ -26,7 +26,15 @@ def main():
     elif menu_result == '2':
         names = setup_players()
         board = setup_board()
-        menu(board)
+
+    '''Starts with white's turn'''
+    game_in_progress = True
+    #while the game hasn't ended, repeat turns
+    colour = 'White'
+    while game_in_progress:
+        start_turn(colour, names)
+        display_board() #not going to work, needs an update_board() function
+        turn(colour)
 
 def clear():
     '''Clearing the screen'''
@@ -42,20 +50,50 @@ def instructions():
         #WORK IN PROGRESS
         #NOT A PRIORITY TO BE WORKED ON
 
+def turn(colour_of_turn):
+    '''Makes the turn to the opposite player, allows the opposite player to move and disallows the player who moved to move on the opponent's turn'''
+    if colour_of_turn == 'Black':
+        colour_of_turn = 'White'
+    else:
+        colour_of_turn = 'Black'
+
+def start_turn(colour,names):
+    '''Tells the player of the turn that it is their turn to make a move. Calls move() so that they can start selecting a move'''
+    if colour == 'White':
+        print(f'{names[0]}, it is your turn to make a move.')
+    else:
+        print(f'{names[1]}, it is your turn to make a move.')
+    move(colour,names)
+
+def move(colour,names):
+    '''Checks if their move is possible and moves it if it is'''
+    if colour == 'White': #conditions for white
+        move = input(f'What is your move, {names[0]}?\n')
+        
+        #needs to check the piece at square
+        #then according to the piece, check for legal moves for that specific piece
+        #if not legal, redo the move
+        #if legal, do the move and display board
+
+
+
+        pass
+    else: #conditions for black
+        pass #work in progress
+
+def check_piece_at_square(square):
+    '''Returns the piece at the square when put into the format  "letternumber" '''
+    pass # work in progress
+
 def setup_players():
     print("Let's play!")
     names = [input('Player 1 (White), enter your name.\n'), input('Player 2 (Black), enter your name.\n')]
     return names
 
 def setup_board():
-    board = random_board(board)
-    display_board(board)
-    return board
-
-def turn(board):
-    '''Inputs names from user - starts up a new board'''
-    
-    
+    '''Creates a new random board'''
+    board = random_board(empty_board()) #creates an empty board, then randomises it
+    display_board(board) #displays the random board
     return board
 
 def empty_board():
@@ -88,7 +126,7 @@ def display_board(board):
 
     print(f"  {column_display}")
 
-def random_board(board):
+def random_board(random_board):
     '''Shuffles the pieces, setting the whole row of white and black pieces to the 8th and 1st rank with board[-1] and board[0]'''
     white_shuffle = ['♔', '♕', '♖', '♖', '♗', '♗', '♘', '♘']
     black_shuffle = ['♚', '♛', '♜', '♜', '♝', '♝', '♞', '♞']
@@ -96,12 +134,12 @@ def random_board(board):
     random.shuffle(white_shuffle)
     random.shuffle(black_shuffle)
 
-    board[-1] = white_shuffle
-    board[0] = black_shuffle
+    random_board[-1] = white_shuffle
+    random_board[0] = black_shuffle
 
-    board[-2] = ['♙'] * 8 #makes the 2nd row from the bottoms pawns
-    board[1] = ['♟'] * 8 #makes the 2nd row from the top pawns
-    return board
+    random_board[-2] = ['♙'] * 8 #makes the 2nd row from the bottoms pawns
+    random_board[1] = ['♟'] * 8 #makes the 2nd row from the top pawns
+    return random_board
 
 def menu():
     '''Starts up a menu to ask the user the instruction to do, then executes based on their choice'''
@@ -112,4 +150,4 @@ def menu():
         menu_result = input('Welcome to Chess 960!\n1. Open instructions\n2. Start new game\nEnter what you want to do: ')
     return menu_result
 
-menu()
+main()
