0dcf476a19ab4ecdc487e8f3e0f96c4d90d21364
batonandadrian <134459797+batonandadrian@users.noreply.github.com>
2025-06-09 12:26:59 +0000

    Jun 9, 2025, 12:26 PM

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def pieces_in_between(start,end,mode,board):
         else:
             step = 1
             #iterate forwards
-        for row in range(start_row,end_row,step):
+        for row in range(start_row,end_row + 1,step):
             pieces_between.append(check_piece_at_square(str(start_row+step)+start_column,'numnum'))
             
         print(pieces_between)
