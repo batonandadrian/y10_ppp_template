71b0a232b1b15d5e57d1cf9a62e20bd265e556cd
batonandadrian <134459797+batonandadrian@users.noreply.github.com>
2025-06-15 13:37:27 +0000

    Jun 15, 2025, 1:37 PM

--- main.py
+++ main.py
@@ -111,6 +111,7 @@ def move(colour,names,board):
     if colour == 'White': #conditions for white
         conditions_met = False
         while conditions_met == False:  #repeatedly asks user squares until all the requirements are fulfilled
+            display_board()
             test('white')
             start_square = input(f'Where is your starting square, {names[0]}?\n').lower()
             print(f'I see you are trying to move the {check_piece_at_square(start_square,board)}  on {start_square}.')
